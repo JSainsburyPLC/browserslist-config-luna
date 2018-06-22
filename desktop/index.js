f1f6d859b1f316a28449c27e02136b185f2331d8
Will Bamford <w.bamford@gmail.com>
2018-06-22 11:52:25 +0100

    Update index.js

--- desktop/index.js
+++ desktop/index.js
@@ -1,9 +1,9 @@
 module.exports = [
-  "Chrome 63", // colleague specific
+  "Chrome >= 63", // colleague specific
   "last 2 chrome versions",
   "ie >= 10",
   "last 1 edge version",
-  "Firefox 56", // colleague specific
+  "Firefox >= 56", // colleague specific
   "last 1 firefox version",
   "last 1 safari version",
 ]
